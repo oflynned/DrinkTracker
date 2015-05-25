3eca371d1117e7645d215f8ccad50d238e0fba50
Ed <oflynned@tcd.ie>
2015-05-25 16:46:26 +0100

    intro screen and working intents

--- app/src/main/java/com/glassbyte/drinktracker/IntroActivity.java
+++ app/src/main/java/com/glassbyte/drinktracker/IntroActivity.java
@@ -1,4 +1,35 @@
 package com.glassbyte.drinktracker;
 
-public class IntroActivity {
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RadioButton;
+
+
+public class IntroActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radioButtonMale:
+                if (checked)
+
+                    break;
+            case R.id.radioButtonFemale:
+                if (checked)
+
+                    break;
+        }
+    }
 }
--- app/src/main/java/com/glassbyte/drinktracker/MainActivity.java
+++ app/src/main/java/com/glassbyte/drinktracker/MainActivity.java
@@ -1,17 +1,22 @@
 package com.glassbyte.drinktracker;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this,IntroActivity.class);
+        startActivity(intent);
     }
 
     @Override
