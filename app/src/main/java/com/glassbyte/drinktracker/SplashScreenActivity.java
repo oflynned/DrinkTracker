3a757972f02f328ad9ee72ae42040d45bbc6fb56
Ed <oflynned@tcd.ie>
2015-05-25 16:54:13 +0100

    updated layout

--- app/src/main/java/com/glassbyte/drinktracker/SplashScreenActivity.java
+++ app/src/main/java/com/glassbyte/drinktracker/SplashScreenActivity.java
@@ -23,7 +23,7 @@ public class SplashScreenActivity extends Activity {
             public void run() {
                 try {
                     int timer = 0;
-                    while (timer < 2000) {
+                    while (timer < 1000) {
                         sleep(100);
                         timer = timer + 100;
                     }
