190e3c8bb6760e4cf044b51c373f470aa451d8f4
Ed <oflynned@tcd.ie>
2015-05-25 15:29:18 +0100

    splash screen

--- app/src/main/java/com/glassbyte/drinktracker/SplashScreenActivity.java
+++ app/src/main/java/com/glassbyte/drinktracker/SplashScreenActivity.java
@@ -1,7 +1,63 @@
 package com.glassbyte.drinktracker;
 
-/**
- * Created by ed on 25/05/15.
- */
-public class SplashScreenActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreenActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread splash = new Thread() {
+            public void run() {
+                try {
+                    int timer = 0;
+                    while (timer < 2000) {
+                        sleep(100);
+                        timer = timer + 100;
+                    }
+                    startActivity(new Intent("com.glassbyte.drinktracker.ClearScreen"));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    finish();
+                }
+            }
+        };
+        splash.start();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
+
