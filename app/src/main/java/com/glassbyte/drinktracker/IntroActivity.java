c903267309145155e3b8a673970213044784bd30
Ed <oflynned@tcd.ie>
2015-05-25 18:06:00 +0100

    intro screen almost done

--- app/src/main/java/com/glassbyte/drinktracker/IntroActivity.java
+++ app/src/main/java/com/glassbyte/drinktracker/IntroActivity.java
@@ -1,19 +1,43 @@
 package com.glassbyte.drinktracker;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 
-public class IntroActivity extends ActionBarActivity {
+public class IntroActivity extends ActionBarActivity{
+
+    RadioButton mGender;
+    EditText mWeight;
+    EditText mHeight;
+    Button btnContinue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        mWeight = (EditText) findViewById(R.id.weightET);
+        mHeight = (EditText) findViewById(R.id.heightET);
+        btnContinue = (Button) findViewById(R.id.buttonContinue);
+
+        btnContinue.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+
+                Toast.makeText(getBaseContext(),"Button clicked",Toast.LENGTH_SHORT).show();
+                //Intent intent = new Intent(this,AddDrinkActivity.class);
+                //startActivity(intent);;
+            }
+        });
     }
 
     public void onRadioButtonClicked(View view) {
