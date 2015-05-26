ad309db52aab7e28b4bc8c0d3c48ea4b8d237a15
Ed <oflynned@tcd.ie>
2015-05-26 17:57:13 +0100

    working sharedprefs

--- app/src/main/java/com/glassbyte/drinktracker/IntroActivity.java
+++ app/src/main/java/com/glassbyte/drinktracker/IntroActivity.java
@@ -10,15 +10,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 
 public class IntroActivity extends ActionBarActivity{
     SharedPreferencesActivity sharedPreference;
 
-    RadioButton mGender;
+    private boolean checked;
+
+    String mGender;
     EditText mWeight;
     EditText mHeight;
+    RadioGroup mRadioGroup;
     Button btnContinue;
 
     @Override
@@ -30,36 +34,67 @@ public class IntroActivity extends ActionBarActivity{
 
         mWeight = (EditText) findViewById(R.id.weightET);
         mHeight = (EditText) findViewById(R.id.heightET);
+        mRadioGroup = (RadioGroup) findViewById(R.id.genderGroup);
         btnContinue = (Button) findViewById(R.id.buttonContinue);
 
         btnContinue.setOnClickListener(new View.OnClickListener()
         {
             public void onClick(View v)
             {
-                String run = "true";
 
-                sharedPreference.save(getBaseContext(),run);
-                Toast.makeText(getBaseContext(),"SharedPreference saved: " + run,Toast.LENGTH_SHORT).show();
+                if(mHeight.getText().toString().trim().length() == 0){
+                    Toast.makeText(getBaseContext(),"Please fill in all details",Toast.LENGTH_SHORT).show();
+                }
 
-                //Intent intent = new Intent(this,MainActivity.class);
-                //startActivity(intent);
+                if(mWeight.getText().toString().trim().length() == 0){
+                    Toast.makeText(getBaseContext(),"Please fill in all details",Toast.LENGTH_SHORT).show();
+                }
+
+                if (mRadioGroup.getCheckedRadioButtonId() == -1)
+                {
+                    Toast.makeText(getBaseContext(),"Please fill in all details",Toast.LENGTH_SHORT).show();
+                }
+
+                else {
+
+                    String gender = mGender;
+
+                    //height
+                    String height = mHeight.getText().toString();
+                    int fHeight = Integer.parseInt(height);
+
+                    //weight
+                    String weight = mWeight.getText().toString();
+                    int fWeight = Integer.parseInt(weight);
+
+                    String run = "true";
+
+                    sharedPreference.save(getBaseContext(), run);
+                    sharedPreference.save(getBaseContext(), gender);
+                    sharedPreference.save(getBaseContext(), weight);
+                    sharedPreference.save(getBaseContext(), height);
+
+                    Toast.makeText(getBaseContext(), "SharedPreference saved: " + run, Toast.LENGTH_SHORT).show();
+
+                    //Intent intent = new Intent(this,MainActivity.class);
+                    //startActivity(intent);
+                }
             }
         });
     }
 
     public void onRadioButtonClicked(View view) {
-        // Is the button now checked?
-        boolean checked = ((RadioButton) view).isChecked();
 
+        checked = ((RadioButton) view).isChecked();
         // Check which radio button was clicked
         switch(view.getId()) {
             case R.id.radioButtonMale:
                 if (checked)
-                    //male
+                    mGender = "male";
                     break;
             case R.id.radioButtonFemale:
                 if (checked)
-                    //female
+                    mGender= "female";
                     break;
         }
     }
