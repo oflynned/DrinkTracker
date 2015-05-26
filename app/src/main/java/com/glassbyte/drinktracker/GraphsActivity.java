2634929894bc67440f6de0fbb49ddaa7dec051b7
Alexandru-S <suleaa@tcd.ie>
2015-05-26 16:56:55 +0100

    graphing activity

--- app/src/main/java/com/glassbyte/drinktracker/GraphsActivity.java
+++ app/src/main/java/com/glassbyte/drinktracker/GraphsActivity.java
@@ -2,6 +2,41 @@ package com.glassbyte.drinktracker;
 
 /**
  * Created by ed on 25/05/15.
+ * Edited by Alex on 25/05/15
  */
-public class GraphsActivity {
+import android.graphics.Color;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+
+public class GraphsActivity extends Activity{
+//called when activity is first created
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graphs);
+
+    }
+    public void lineGraphHandler(View view) {
+
+    }
+    public void barGraphHandler(View view){
+
+    }
+    public void pieGraphHandler(View view){
+
+    }
+    public void sceatterGraphHandler(View view){
+
+    }
+
+
+
+
 }
