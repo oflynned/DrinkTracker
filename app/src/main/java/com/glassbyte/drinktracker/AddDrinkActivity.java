a2931323c405cb72b8b983be174aabe59511c185
mashikag <littlesoldiergraba@gmail.com>
2015-05-27 18:29:42 +0100

    Added Preset Drinks fragment also added layouts for both the PresetDrinks and the AddDrink FragmentActivity, after continue is pressed it will point you to the AddDrink fragment however for now it crashes onCreate. @

--- app/src/main/java/com/glassbyte/drinktracker/AddDrinkActivity.java
+++ app/src/main/java/com/glassbyte/drinktracker/AddDrinkActivity.java
@@ -1,7 +1,53 @@
 package com.glassbyte.drinktracker;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 
 /**
  * Created by ed on 25/05/15.
  */
-public class AddDrinkActivity {
+public class AddDrinkActivity extends FragmentActivity{
+    private static final int NUM_PAGES = 3;
+    private ViewPager mPager;
+    private PagerAdapter mPagerAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adddrink);
+
+        //Instantiate a ViewPager and a PAgerAdapter
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(mPager.getCurrentItem() == 1){
+            super.onBackPressed();
+        } else {
+            mPager.setCurrentItem(1);
+        }
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter{
+        public ScreenSlidePagerAdapter(FragmentManager fm){
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position){
+            return new PresetDrink();
+        }
+
+        @Override
+        public int getCount(){
+            return NUM_PAGES;
+        }
+    }
 }
---
+++ app/src/main/java/com/glassbyte/drinktracker/PresetDrink.java
@@ -0,0 +1,22 @@
+package com.glassbyte.drinktracker;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by root on 27/05/15.
+ */
+public class PresetDrink extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.activity_presetdrink, container, false);
+
+        return rootView;
+    }
+}
