7341a552995734ba2bee44c06d4f1cadbaf58bb2
Alexandru-S <suleaa@tcd.ie>
2015-05-30 01:24:38 +0100

    Dyn graph with xml-no data link

--- app/src/main/java/com/glassbyte/drinktracker/RealTimeActivity.java
+++ app/src/main/java/com/glassbyte/drinktracker/RealTimeActivity.java
@@ -5,70 +5,87 @@ package com.glassbyte.drinktracker;
  *
  */
 
-import android.graphics.Color;
+import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.LegendRenderer;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
-
+import java.util.Random;
 
 
 
 public class RealTimeActivity extends android.support.v4.app.Fragment {
 
+    private final Handler mHandler = new Handler();
+
+    private Runnable mTimer1;
+    private Runnable mTimer2;
+    private LineGraphSeries<DataPoint>mSeries1;
+    private LineGraphSeries<DataPoint>mSeries2;
+    private double graph2LastXValue =5d;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.activity_realtime, container, false);
         GraphView graph = (GraphView) rootView.findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[]
-                {
-                        new DataPoint(0, 1),
-                        new DataPoint(1, 5),
-                        new DataPoint(2, 3),
-                        new DataPoint(3, 4),
-                        new DataPoint(4, 6),
-                        new DataPoint(5, 3)
-                });
-        graph.addSeries(series);
-
-        LineGraphSeries<DataPoint> series2 = new LineGraphSeries<DataPoint>(new DataPoint[]
-                {
-                        new DataPoint(0, 0),
-                        new DataPoint(1, 3),
-                        new DataPoint(2, 2),
-                        new DataPoint(3, 6),
-                        new DataPoint(4, 7),
-                        new DataPoint(5, 6)
-                });
-        graph.addSeries(series2);
-
-
-        //edditing graph
-        series.setTitle("foo");
-        series.setThickness(20);
-
-
-        series2.setTitle("bar");
-        series.setThickness(10);
-
-
-
-
-        graph.getLegendRenderer().setVisible(true);
-        graph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.BOTTOM);
-        //graph.getFitsSystemWindows();
-        graph.setBackgroundColor(Color.LTGRAY);
-        graph.setTitleColor(Color.BLACK);
-        graph.setTitle("GRAPH TITLE");
-
-
+        mSeries1=new LineGraphSeries<DataPoint>(generateData());
+        graph.addSeries(mSeries1);
         return rootView;
+
     }
+
+    @Override
+    public void onAttach(Activity activity)
+    {
+        super.onAttach( activity);
+        mTimer1 = new Runnable() {
+            @Override
+            public void run() {
+                mSeries1.resetData(generateData());
+                mHandler.postDelayed(this,300);
+            }
+        };
+        mHandler.postDelayed(mTimer1, 300);
+
+    }
+    @Override
+    public void onPause()
+    {
+        mHandler.removeCallbacks(mTimer1);
+        super.onPause();
+
+
+
+    }
+    private DataPoint[] generateData()
+    {
+        int count = 30;
+        DataPoint[] values = new DataPoint[count];
+        for(int i=0;i<count;i++)
+        {
+            double x =i;
+            double f = mRand.nextDouble()*0.15+0.3;
+            double y = Math.sin(i*f+2) + mRand.nextDouble()*0.3;
+
+            DataPoint v = new DataPoint(x, y);
+            values[i] = v;
+        }
+        return values;
+    }
+
+    double mLastRandom = 2;
+    Random mRand = new Random();
+    private double getRandom() {
+        return mLastRandom += mRand.nextDouble()*0.5 - 0.25;
+    }
+
+
 }
 
