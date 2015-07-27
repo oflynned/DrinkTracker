63bf34e593174d8fd757ebf655f9b034ca081cab
Ed <oflynned@tcd.ie>
2015-07-27 14:41:14 +0100

    added spinners and have to finish choice of percentage allocation by the first spinner's selection as of currently it only allocates beer percentages to all choices which is unacceptable

--- app/src/main/java/com/glassbyte/drinktracker/PresetDrink.java
+++ app/src/main/java/com/glassbyte/drinktracker/PresetDrink.java
@@ -3,8 +3,6 @@ package com.glassbyte.drinktracker;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.internal.widget.AdapterViewCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +10,8 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
+
 
 /**
  * Created by root on 27/05/15.
@@ -23,7 +23,9 @@ public class PresetDrink extends Fragment implements View.OnClickListener {
     private DatabaseOperationsUnits DOU;
     private Cursor CR;
     private ImageView glass;
-    private Spinner spinner;
+    private Spinner drinksChoice;
+    private Spinner percentageChoice;
+    private TextView addPercentageText;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -32,25 +34,110 @@ public class PresetDrink extends Fragment implements View.OnClickListener {
 
         glass = (ImageView) V.findViewById(R.id.presetDrink);
 
-        spinner = (Spinner) V.findViewById(R.id.spinnerPresetDrink);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        addPercentageText = (TextView) V.findViewById(R.id.percentagePreset);
+        addPercentageText.setVisibility(View.GONE);
+
+        percentageChoice = (Spinner) V.findViewById(R.id.spinnerPresetDrinkPercentage);
+        percentageChoice.setVisibility(View.GONE);
+
+        drinksChoice = (Spinner) V.findViewById(R.id.spinnerPresetDrink);
+        drinksChoice.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
             @Override
             public void onItemSelected(AdapterView<?> arg0, View arg1,
                                        int arg2, long arg3) {
 
-                int position = spinner.getSelectedItemPosition();
+                int position = drinksChoice.getSelectedItemPosition();
                 switch (position){
-                    //beer
+                    //null
                     case 0:
-                        glass.setImageResource(R.drawable.beer_bottle);
+                        glass.setImageResource(R.drawable.ic_launcher);
+                        addPercentageText.setVisibility(View.GONE);
+                        percentageChoice.setVisibility(View.GONE);
                         break;
-                    //wine
+                    //beer can
                     case 1:
-                        glass.setImageResource(R.drawable.glass_wine);
+                        glass.setImageResource(R.drawable.beer_can);
+                        addPercentageText.setVisibility(View.VISIBLE);
+                        percentageChoice.setVisibility(View.VISIBLE);
                         break;
+                    //beer bottle
                     case 2:
+                        glass.setImageResource(R.drawable.beer_bottle);
+                        addPercentageText.setVisibility(View.VISIBLE);
+                        percentageChoice.setVisibility(View.VISIBLE);
+                        break;
+                    //beer can
+                    case 3:
+                        glass.setImageResource(R.drawable.beer_can);
+                        addPercentageText.setVisibility(View.VISIBLE);
+                        percentageChoice.setVisibility(View.VISIBLE);
+                        break;
+                    //cider bottle
+                    case 4:
+                        glass.setImageResource(R.drawable.beer_can);
+                        addPercentageText.setVisibility(View.VISIBLE);
+                        percentageChoice.setVisibility(View.VISIBLE);
+                        break;
+                    //cider bottle
+                    case 5:
+                        glass.setImageResource(R.drawable.beer_can);
+                        addPercentageText.setVisibility(View.VISIBLE);
+                        percentageChoice.setVisibility(View.VISIBLE);
+                        break;
+                    //cider can
+                    case 6:
+                        glass.setImageResource(R.drawable.beer_can);
+                        addPercentageText.setVisibility(View.VISIBLE);
+                        percentageChoice.setVisibility(View.VISIBLE);
+                        break;
+                    //glass of wine 150ml
+                    case 7:
+                        glass.setImageResource(R.drawable.glass_wine);
+                        addPercentageText.setVisibility(View.VISIBLE);
+                        percentageChoice.setVisibility(View.VISIBLE);
+                        break;
+                    //glass of wine 300ml
+                    case 8:
+                        glass.setImageResource(R.drawable.glass_wine);
+                        addPercentageText.setVisibility(View.VISIBLE);
+                        percentageChoice.setVisibility(View.VISIBLE);
+                        break;
+                    //single shot
+                    case 9:
                         glass.setImageResource(R.drawable.shot);
+                        addPercentageText.setVisibility(View.VISIBLE);
+                        percentageChoice.setVisibility(View.VISIBLE);
+                        break;
+                    //double shot
+                    case 10:
+                        glass.setImageResource(R.drawable.shot);
+                        addPercentageText.setVisibility(View.VISIBLE);
+                        percentageChoice.setVisibility(View.VISIBLE);
+                        break;
+                    //triple shot
+                    case 11:
+                        glass.setImageResource(R.drawable.shot);
+                        addPercentageText.setVisibility(View.VISIBLE);
+                        percentageChoice.setVisibility(View.VISIBLE);
+                        break;
+                    //330ml alcopop
+                    case 12:
+                        glass.setImageResource(R.drawable.beer_bottle);
+                        addPercentageText.setVisibility(View.VISIBLE);
+                        percentageChoice.setVisibility(View.VISIBLE);
+                        break;
+                    //330ml alcopop
+                    case 13:
+                        glass.setImageResource(R.drawable.beer_bottle);
+                        addPercentageText.setVisibility(View.VISIBLE);
+                        percentageChoice.setVisibility(View.VISIBLE);
+                        break;
+                    //cocktail
+                    case 14:
+                        glass.setImageResource(R.drawable.glass_water);
+                        addPercentageText.setVisibility(View.GONE);
+                        percentageChoice.setVisibility(View.GONE);
                         break;
                 }
             }
@@ -62,6 +149,7 @@ public class PresetDrink extends Fragment implements View.OnClickListener {
             }
 
         });
+
 
         addDrink = (Button) V.findViewById(R.id.presetAddDrink);
         addDrink.setOnClickListener(this);
