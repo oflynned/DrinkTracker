e82c8d37c4a3b14a1bd13d58aa76f6b7ba293d18
mashikag <littlesoldiergraba@gmail.com>
2015-08-19 01:28:08 +0100

    ListDrinks now lists all the added drinks to the database as well as associates a checkbox with each row. Also added select all button and already implemented functionality for it. Next need to implement functionality for the remove selected button and also make it to display certain constant maximum of drinks per page so it won't overflow with drinks in case more are being stored in the database.

--- app/src/main/java/com/glassbyte/drinktracker/ListDrinksActivity.java
+++ app/src/main/java/com/glassbyte/drinktracker/ListDrinksActivity.java
@@ -2,6 +2,7 @@ package com.glassbyte.drinktracker;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -15,7 +16,11 @@ import android.widget.GridView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class ListDrinksActivity extends AppCompatActivity {
+    DrinksListAdapter drinkListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +28,10 @@ public class ListDrinksActivity extends AppCompatActivity {
         setContentView(R.layout.activity_list_drinks);
 
         GridView gridView = (GridView)findViewById(R.id.listDrinksGridView);
-        gridView.setAdapter(new DrinksListAdapter(this));
+
+        drinkListAdapter = new DrinksListAdapter(this);
+
+        gridView.setAdapter(drinkListAdapter);
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v,
                                     int position, long id) {
@@ -55,20 +63,38 @@ public class ListDrinksActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void selectAll(View view){
+        /*CheckBox[] checkboxes = drinkListAdapter.getCheckboxes();
+
+        for (CheckBox checkbox: checkboxes) {
+            checkbox.setEnabled(true);
+        }
+        */
+        System.out.println("was here");
+        drinkListAdapter.selectAllCheckboxes();
+    }
+
+    public void removeSelected(View view){
+
+    }
+
     public class DrinksListAdapter extends BaseAdapter{
         private Context mContext;
         private DatabaseOperationsUnits dou;
         private Cursor result;
         private final int NUM_COLUMNS = 5;
+        private ArrayList<CheckBox> drinkCheckboxes;
 
         public DrinksListAdapter(Context c){
             mContext = c;
             dou = new DatabaseOperationsUnits(mContext);
+            drinkCheckboxes = new ArrayList<>();
+            result = dou.getReadableDatabase().rawQuery("SELECT * FROM " + DataUnitsDatabaseContractor.DataLoggingTable.TABLE_NAME, null);
+            result.moveToFirst();
         }
 
         @Override
         public int getCount() {
-            result = dou.getReadableDatabase().rawQuery("SELECT * FROM " + DataUnitsDatabaseContractor.DataLoggingTable.TABLE_NAME, null);
             return result.getCount() * 5 + NUM_COLUMNS; //5 columns to be displayed in the list for each row in the database + 5 for the top row
         }
 
@@ -84,37 +110,80 @@ public class ListDrinksActivity extends AppCompatActivity {
 
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
-            View test = null;
+            View cellView = null;
+
+            /*if (!result.isBeforeFirst())
+                result.moveToFirst();
+            */
             if (i < NUM_COLUMNS) {
-                test = new TextView(mContext);
+                cellView = new TextView(mContext);
                switch (i) {
                    case 0:
-                       ((TextView)test).setText("Select");
+                       ((TextView)cellView).setText("Select");
                        break;
                    case 1:
-                       ((TextView)test).setText("Date"); 
+                       ((TextView)cellView).setText("Date");
                        break;
                    case 2:
-                       ((TextView)test).setText("Title");
+                       ((TextView)cellView).setText("Title");
                        break;
                    case 3:
-                       ((TextView)test).setText("Volume");
+                       ((TextView)cellView).setText("Volume");
                        break;
                    case 4:
-                       ((TextView)test).setText("AlcVol");
+                       ((TextView)cellView).setText("AlcVol");
                        break;
                }
-                   
+
+                ((TextView)cellView).setAllCaps(true);
+                ((TextView)cellView).setTypeface(null, Typeface.BOLD);
+
             } else {
+                result.moveToPosition(i/NUM_COLUMNS-1);
                 if ((i % NUM_COLUMNS) == 0) {
-                    test = new CheckBox(mContext);
+                    cellView = new CheckBox(mContext);
+                    int rowId = result.getInt(0); //index 0 == id column
+                    cellView.setId(rowId);
+                    drinkCheckboxes.add((CheckBox)cellView);
                 }
                 else {
-                    test = new TextView(mContext);
-                    ((TextView) test).setText("Pos: " + i);
+                    int colPos = i%NUM_COLUMNS;
+                    cellView = new TextView(mContext);
+                    ((TextView) cellView).setText(result.getString(colPos)
+                            + ((colPos == 3) ? "ml" : "")
+                            + ((colPos == 4) ? "%" : ""));
+
+                    if((i % NUM_COLUMNS) == (NUM_COLUMNS-1))
+                        result.moveToNext();
                 }
             }
-            return test;
+            return cellView;
+        }
+
+        public CheckBox[] getCheckboxes(){
+            CheckBox[] drinkCheckboxesArray = new CheckBox[drinkCheckboxes.size()];
+            drinkCheckboxesArray = drinkCheckboxes.toArray(drinkCheckboxesArray);
+            return drinkCheckboxesArray;
+        }
+
+        public void selectAllCheckboxes(){
+            boolean alreadyAllSelected = true;
+            Iterator<CheckBox> itr = drinkCheckboxes.iterator();
+            while(itr.hasNext()){
+                CheckBox checkbox = itr.next();
+                if(!checkbox.isChecked()){
+                    alreadyAllSelected = false;
+                    checkbox.setChecked(true);
+                }
+            }
+
+            //If all were already selected then deselect all
+            if(alreadyAllSelected){
+                itr = drinkCheckboxes.iterator();
+                while(itr.hasNext()){
+                    itr.next().setChecked(false);
+                }
+            }
         }
     }
 }
